23dd72050c19f776f647f51207f374dee0216f23
Philip Vuong <40782386+philipvuong@users.noreply.github.com>
2021-02-11 08:52:53 -0600

    cleanup & PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,42 +32,6 @@ self.addEventListener("install", function(event) {
 
 // Respond with cached resources
 self.addEventListener("fetch", function(event) {
-  // cache all get requests to /api routes
-  // if (event.request.url.includes("/api/")) {
-  //   event.respondWith(
-  //     caches.open(DATA_CACHE_NAME).then(cache => {
-  //       return fetch(event.request)
-  //         .then(response => {
-  //           // If the response was good, clone it and store it in the cache.
-  //           if (response.status === 200) {
-  //             cache.put(event.request.url, response.clone());
-  //           }
-
-  //           return response;
-  //         })
-  //         .catch(err => {
-  //           // Network request failed, try to get it from the cache.
-  //           return cache.match(event.request);
-  //         });
-  //     }).catch(err => console.log(err))
-  //   );
-
-  //   return;
-  // }
-
-  // event.respondWith(
-  //   fetch(event.request).catch(function() {
-  //     return caches.match(event.request).then(function(response) {
-  //       if (response) {
-  //         return response;
-  //       } else if (event.request.headers.get("accept").includes("text/html")) {
-  //         // return the cached home page for all requests for html pages
-  //         return caches.match("/");
-  //       }
-  //     });
-  //   })
-  // );
-
   event.respondWith(
     caches.match(event.request).then(function (request) {
       if (request) {
